b221f6d9dee39ca52c68123f78d21fe70633778d
JaeIn <sm810403@hotmail.com>
2020-07-27 21:03:59 -0700

    first commit

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ btn2.addEventListener('click', ()=> {
 });
 
 
-//create ball
-//draw ball
-//create paddle
-//draw paddle
+//create ball/
+//draw ball/
+//create paddle/
+//draw paddle/
 //draw score
 //create brick
 //create bricks
@@ -29,7 +29,7 @@ btn2.addEventListener('click', ()=> {
 //update score and reset
 const ball = {
     x: canvas.width /2,
-    y: canvas.height/2,
+    y: canvas.height/2 +80,
     radius: 10,
     speed: 8,
     dx: 4,
@@ -42,8 +42,68 @@ function drawBall(){
     ctx.fill();
     ctx.closePath();
 };
-//
+//move ball 
+function moveBall(){
+    ball.x +=dx;
+    ball.y +=dy;
+}
 
+//draw score
+var score = 0;
+function drawScore(){
+    ctx.font = '20px arial';
+    ctx.fillText(`Score: ${score}`,canvas.width - 100, 30);
+}
+//create paddle
+const paddle = {
+    x: canvas.width/2-50,
+    y: canvas.height- 40,
+    w: 100,
+    h: 20,
+    speed: 8,
+    dx: 0,
+}
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddle.x, paddle.y, paddle.w,paddle.h);
+    ctx.fillStyle = '#0095dd';
+    ctx.fill();
+    ctx.closePath();
+};
+
+//create brick
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true,
+}
+//creat many bricks
+const numberOfRow = 9;
+const numberOfColumn = 5;
+const bricks = [];
+for (let i=0; i<numberOfRow; i++){
+    bricks[i]=[];
+    for (let j=0;j<numberOfColumn; j++){
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+        const y = j * (brickInfo.y + brickInfo.padding) + brickInfo.offsetY;
+        bricks[i][j]={x,y,...brickInfo};
+    }
+};
+//draw bricks
+function drawBricks(){
+    bricks.forEach(column => {
+        column.forEach(brick=>{
+            ctx.beginPath();
+            ctx.fillStyle = brick.visible? '#0095dd':'transparent';
+            ctx.rect(brick.x,brick.y,brick.w,brick.h);
+            ctx.fill();
+            ctx.closePath();
+        })
+    })
+}
 
 
 
@@ -52,5 +112,17 @@ function drawBall(){
 //draw all
 function draw(){
     drawBall();
+    drawPaddle();
+    drawBricks();
+    drawScore();
 };
-draw();
+
+//animate
+function animate(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    draw();
+    moveBall();
+    requestAnimationFrame(animate);
+
+}
+animate();
